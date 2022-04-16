60231a66ebac8e2806edbae708964f19f2c47051
David-ODB57 <70941844+David-ODB57@users.noreply.github.com>
2022-04-16 20:50:22 +0200

    Update liste.js
    Ajout d'un écouteur d'évènement sur le bouton ajouter sur la page liste.
    Ajout de l'appel de la fonction gérant la géolocalisation.

--- src/assets/script/liste.js
+++ src/assets/script/liste.js
@@ -1,5 +1,5 @@
 import { getAllItems } from "./fetchers.js";
-import { Weather } from "./classes/class.weather.js";
+import { weatherLoc } from "./helpers.js";
 
 const headers = new Headers();
 let url = "/liste";
@@ -11,43 +11,13 @@ let options = {
 };
 
 document.querySelector("body").style.overflow = "visible";
+document.querySelector("#geolocalisation").addEventListener("click", (e) => {
+  e.preventDefault();
+  weatherLoc();
+});
 document.querySelector("#add-item").addEventListener("click", (e) => {
   e.preventDefault();
   location.href = `create.html`;
 });
 
 getAllItems(url, options);
-
-///////////////////////////////
-// Configuration Weather App //
-///////////////////////////////
-
-// Test si la géolocation activé ou non sur le navigateur
-// Utilise les coordonnées du lieu où se situe l'utilisateur
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    (pos) => {
-      new Weather(pos.coords.latitude, pos.coords.longitude);
-    },
-    (error) => {
-      switch (error.code) {
-        case error.PERMISSION_DENIED:
-          alert(error.message);
-          break;
-        case error.POSITION_UNAVAILABLE:
-          alert(error.message);
-          break;
-        case error.TIMEOUT:
-          alert(error.message);
-          break;
-      }
-    },
-    {
-      timeout: 5000,
-      maximumAge: 0,
-      enableHighAccuracy: true,
-    }
-  );
-} else {
-  alert("Veillez activer la géolocalisation afin d'afficher la météo");
-}
